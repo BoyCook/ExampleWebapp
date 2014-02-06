ba770e529476411aeba0035f9a858d4d7b5c2e67
Craig Cook <boycook@me.com>
2014-02-06 08:20:51 +0000

    Moved person into main package

--- src/test/java/org/cccs/dtd/example/TestPerson.java
+++ src/test/java/org/cccs/dtd/example/TestPerson.java
@@ -2,6 +2,10 @@ package org.cccs.dtd.example;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 /**
  * Created by boycook on 06/02/2014.
  */
@@ -10,6 +14,9 @@ public class TestPerson {
     @Test
     public void constructorShouldWork() {
         Person p = new Person("Bob", "Smith", 20);
-    }
 
+        assertThat(p.getFirstName(), is(equalTo("Bob")));
+        assertThat(p.getLastName(), is(equalTo("Smith")));
+        assertThat(p.getAge(), is(equalTo(20)));
+    }
 }
