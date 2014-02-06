fb5be86d20aed95f9abb0737d1e0457de52a8872
Craig Cook <boycook@me.com>
2014-02-06 08:22:56 +0000

    100% coverage

--- src/test/java/org/cccs/dtd/example/TestPerson.java
+++ src/test/java/org/cccs/dtd/example/TestPerson.java
@@ -14,9 +14,19 @@ public class TestPerson {
     @Test
     public void constructorShouldWork() {
         Person p = new Person("Bob", "Smith", 20);
-
         assertThat(p.getFirstName(), is(equalTo("Bob")));
         assertThat(p.getLastName(), is(equalTo("Smith")));
         assertThat(p.getAge(), is(equalTo(20)));
+    }
+
+    @Test
+    public void settersShouldOverrideConstructor() {
+        Person p = new Person("Bob", "Smith", 20);
+        p.setFirstName("Jack");
+        p.setLastName("Jones");
+        p.setAge(30);
+        assertThat(p.getFirstName(), is(equalTo("Jack")));
+        assertThat(p.getLastName(), is(equalTo("Jones")));
+        assertThat(p.getAge(), is(equalTo(30)));
     }
 }
